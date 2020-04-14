14f7c5a26295dd93ec5ee2045b5eae3df9b4e7d8
flint80@yandex.ru <flint80@yandex.ru>
2020-04-14 19:23:02 +0300

    repair

--- build-scripts/build.gradle.kts
+++ build-scripts/build.gradle.kts
@@ -1,1 +1,19 @@
-//test12
+//repositories {
+//    mavenCentral() // or jcentrer
+//    jcenter()
+//    maven {
+//        setUrl("https://plugins.gradle.org/m2/")
+//    }
+//}
+//
+//
+//apply{
+//    plugin("kotlin")
+//}
+//
+//dependencies{
+//    "compile"(files("../lib/spf-1.0.jar"))
+//    "compile"("com.github.node-gradle:gradle-node-plugin:2.2.3")
+//    "compile"("junit:junit:4.12")
+//    //"compile"("gradle.plugin.com.github.jengelman.gradle.plugins:gradle-processes:0.5.0")
+//}
