cdef61a3a0b9712adf2168b0c23add57c328fc27
flint80@yandex.ru <flint80@yandex.ru>
2021-04-10 17:40:54 +0300

    initialization of final version

--- buildSrc/src/main/kotlin/com/gridnine/jasmine/gradle/plugin/ServerDescriptionExtension.kt
+++
@@ -1,6 +0,0 @@
-package com.gridnine.jasmine.gradle.plugin
-
-open class ServerDescriptionExtension{
-    var spfUpdaterAddress = "localhost"
-    var spfUpdaterPort = 21566
-}
--- plugins/com.gridnine.jasmine.web.core/source/com/gridnine/jasmine/web/core/utils/DateUtils.kt
+++
@@ -1,9 +0,0 @@
-package com.gridnine.jasmine.web.core.utils
-
-import kotlin.js.Date
-
-object DateUtils {
-    fun getDiffInMilliseconds(date1:Date, date2:Date):Int{
-        return date1.asDynamic()-date2.asDynamic()
-    }
-}
