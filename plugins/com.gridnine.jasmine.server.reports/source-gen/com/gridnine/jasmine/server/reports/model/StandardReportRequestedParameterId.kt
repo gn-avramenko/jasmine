22e65167c42896cb36ad543cfef2ca46eb531499
flint80@yandex.ru <flint80@yandex.ru>
2021-04-21 20:41:55 +0300

    report module

---
+++ plugins/com.gridnine.jasmine.server.reports/source-gen/com/gridnine/jasmine/server/reports/model/StandardReportRequestedParameterId.kt
@@ -0,0 +1,20 @@
+/*****************************************************************
+ * Gridnine AB http://www.gridnine.com
+ * Project: jasmine-demo-final
+ * This file is auto generated, don't modify it manually
+ *****************************************************************/
+
+@file:Suppress("unused","RemoveRedundantQualifierName","UNCHECKED_CAST","MemberVisibilityCanBePrivate","RemoveEmptyPrimaryConstructor", "FunctionName")
+
+package com.gridnine.jasmine.server.reports.model
+
+enum class StandardReportRequestedParameterId{
+
+    START_DATE,
+    END_DATE;
+
+
+    override fun toString():String{
+    return com.gridnine.jasmine.common.core.meta.MiscMetaRegistry.get().enums["com.gridnine.jasmine.server.reports.model.StandardReportRequestedParameterId"]?.items?.get(name)?.getDisplayName()?:name
+}
+}
